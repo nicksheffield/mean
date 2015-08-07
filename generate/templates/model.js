832b8909d3007e9fab1c9ae61cd7ebff2620b92d
Nick Sheffield <numbereft@gmail.com>
2015-08-07 14:23:19 +1200

    Add deleted_at to model template

--- generate/templates/model.js
+++ generate/templates/model.js
@@ -11,9 +11,11 @@ var deepPopulate = require('mongoose-deep-populate');
 // ----------------------------------------------------------------------------
 // http://mongoosejs.com/docs/guide.html
 var ObjectId = mongoose.Schema.Types.ObjectId;
+var Mixed = mongoose.Schema.Types.Mixed;
 
 var schema = new mongoose.Schema({
-	created_at: {type: Date, default: new Date()}
+	created_at: {type: Date, default: new Date()},
+	deleted_at: {type: Mixed, default: false}
 	// set up your own schema here
 });
 
@@ -26,18 +28,17 @@ schema.plugin(deepPopulate);
 // ----------------------------------------------------------------------------
 schema.pre('save', function(next){
 	// do stuff before save here.
-	// "this" keyword refers to the <%- name %> doc
+	var doc = this;
+	
 	next();
 });
 
 schema.post('save', function(doc){
 	// do stuff after save here
-	// doc var refers to the <%- name %> doc
 });
 
 schema.post('remove', function(doc){
 	// do stuff after deletion here
-	// doc var refers to the <%- name %> doc
 });
 
 // ----------------------------------------------------------------------------
