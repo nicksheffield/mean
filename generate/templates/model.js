6d3193a232755ff9e8a49b029308d4cfa18212e9
Nick Sheffield <numbereft@gmail.com>
2015-08-10 13:34:11 +1200

    changed schema variable name in model to be more descriptive

--- generate/templates/model.js
+++ generate/templates/model.js
@@ -4,44 +4,45 @@
 var mongoose = require('mongoose');
 var _ = require('lodash');
 var deepPopulate = require('mongoose-deep-populate');
+var Schema = mongoose.Schema;
 
 
 // ----------------------------------------------------------------------------
 // Schema
 // ----------------------------------------------------------------------------
 // http://mongoosejs.com/docs/guide.html
-var ObjectId = mongoose.Schema.Types.ObjectId;
-var Mixed = mongoose.Schema.Types.Mixed;
+var ObjectId = Schema.Types.ObjectId;
+var Mixed = Schema.Types.Mixed;
 
-var schema = new mongoose.Schema({
+var <%- name %>Schema = new Schema({
 	created_at: {type: Date, default: new Date()},
 	deleted_at: {type: Mixed, default: false}
 	// set up your own schema here
 });
 
 // https://github.com/buunguyen/mongoose-deep-populate/blob/master/README.md
-schema.plugin(deepPopulate);
+<%- name %>Schema.plugin(deepPopulate);
 
 
 // ----------------------------------------------------------------------------
 // Hooks
 // ----------------------------------------------------------------------------
-schema.pre('save', function(next){
+<%- name %>Schema.pre('save', function(next){
 	// do stuff before save here.
 	var doc = this;
 	
 	next();
 });
 
-schema.post('save', function(doc){
+<%- name %>Schema.post('save', function(doc){
 	// do stuff after save here
 });
 
-schema.post('remove', function(doc){
+<%- name %>Schema.post('remove', function(doc){
 	// do stuff after deletion here
 });
 
 // ----------------------------------------------------------------------------
 // Model
 // ----------------------------------------------------------------------------
-module.exports = mongoose.model('<%- capitalizedName %>', schema);
+module.exports = mongoose.model('<%- capitalizedName %>', <%- name %>Schema);
