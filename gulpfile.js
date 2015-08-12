7ccc21eb24d64c28aca3ab8fe53580bb6b510200
Nick Sheffield <numbereft@gmail.com>
2015-08-12 13:35:45 +1200

    Improved gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,163 +1,131 @@
-var gulp          = require('gulp');
-var watch         = require('gulp-watch');
-var concat        = require('gulp-concat');
-var concatCss     = require('gulp-concat-css');
-var uglify        = require('gulp-uglify');
-var uglifycss     = require('gulp-uglifycss');
-var autoprefixer  = require('gulp-autoprefixer');
-var stylus        = require('gulp-stylus');
-var size          = require('gulp-filesize');
-var annotate      = require('gulp-ng-annotate');
-var templateCache = require('gulp-angular-templatecache');
-var babel         = require('gulp-babel');
-var gutil         = require('gulp-util');
-var jscs          = require('gulp-jscs');
-var es            = require('event-stream');
-var sq            = require('streamqueue');
-var del           = require('del');
-var vinylPaths    = require('vinyl-paths');
+var del           = require('del')                          // delete files
+var gulp          = require('gulp')                         // the main guy
+var jscs          = require('gulp-jscs')                    // javascript code style
+var babel         = require('gulp-babel')                   // process es2015 syntax
+var order         = require('gulp-order')                   // reorder files in stream
+var insert        = require('gulp-insert')                  // add text to file
+var rename        = require('gulp-rename')                  // rename file
+var concat        = require('gulp-concat')                  // merge files together
+var uglify        = require('gulp-uglify')                  // minify js
+var stylus        = require('gulp-stylus')                  // turn stylus into css
+var addsrc        = require('gulp-add-src')                 // mid-stream gulp.src()
+var plumber       = require('gulp-plumber')                 // handle errors without crashing
+var filelog       = require('gulp-filelog')                 // list all files in the stream
+var stylish       = require('gulp-jscs-stylish')            // make jscs output nicer
+var annotate      = require('gulp-ng-annotate')             // safely minify angular
+var minifycss     = require('gulp-minify-css')              // minify css code
+var sourcemap     = require('gulp-sourcemaps')              // create sourcemaps
+var autoprefix    = require('gulp-autoprefixer')            // prefix any css with low support
+var templateCache = require('gulp-angular-templatecache')   // cache angular template files
 
 
 
+gulp.task('css', function() {
 
-
-gulp.task('styles', ['clean:styles'], function() {
-
-	// this is to gracefully fail stylus
-	var s = stylus();
+	var stream = gulp.src('./public/assets/css/style.styl') // get the source files
+		.pipe(plumber())                                    // stop syntax errors crashing the watch
+		.pipe(sourcemap.init())                             // get ready to write a sourcemap
+		.pipe(stylus())                                     // turn the stylus into css
+		.pipe(sourcemap.write())                            // write the sourcemap
+		.pipe(autoprefix('last 2 versions'))                // autoprefix the css code
+		.pipe(gulp.dest('public/dist/'))                    // save it into the dist folder
+		.pipe(minifycss())                                  // minify it (removes the sourcemap)
+		.pipe(sourcemap.write())                            // write the sourcemap
+		.pipe(rename('style.min.css'))                      // add .min to the filename
+		.pipe(gulp.dest('public/dist/'))                    // save it into the dist folder
 	
-	var sFail = function() {
-		//gutil.log(arguments);
-		
-		console.log('Stylus has a problem');
-		s.end();
-	};
+	return stream
 
-	// add more gulp.src()'s as extra parameters to the es.merge() below to include ordinary css files
-	var stream = es.merge(gulp.src('public/assets/css/style.styl').pipe(s).on('error', sFail))
-		.pipe(concatCss('style.min.css'))
-		.pipe(uglifycss())
-		.pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-		}))
-		.pipe(gulp.dest('public/dist'));
-	
-	return stream;
-	
-});
-
-
+})
 
 
 
 gulp.task('angular', function() {
 	
-	var tplStream = gulp.src('angular/views/*.html')
-		.pipe(templateCache('templates.js', {
-			module: 'app.templates'
-		}));
+	var tplCacheOpts = {
+		module: 'app.templates'
+	}
+
+	var stream = gulp.src('angular/views/*.html')           // grab all the html views
+		.pipe(plumber())                                    // stop any errors from breaking a watch
+		.pipe(templateCache('templates.js', tplCacheOpts))  // make a template cache from them
+		.pipe(insert.prepend('//jscs:disable\n'))           // disable jscs for the template cache
+		.pipe(addsrc(['angular/*.js', 'angular/**/*.js']))  // add the rest of the angular app
+		.pipe(order(['app.js']))                            // make sure app.js is first
+		//.pipe(filelog())                                  // log the files in the stream
+		.pipe(babel())                                      // enable ES2015 support
+		.pipe(jscs())                                       // javascript code style
+		.on('error', function(){})                          // suppress jscs error reporting
+		.pipe(stylish())                                    // third-party jscs error reporting
+		.pipe(annotate())                                   // make angular callbacks minifyable
+		.pipe(uglify())                                     // minify the code
+		.pipe(concat('app.min.js'))                         // merge them all into the same file
+		.pipe(gulp.dest('public/dist/'))                    // save it into the dist folder
+		
+	return stream
 	
-	var jsStream = gulp.src([
-		'angular/app.js',
-		'angular/app.routes.js',
-		'angular/app.auth.js',
-		'angular/**/*.js'
-	])
-		.pipe(babel())
-		//.pipe(jscs())
-		.pipe(annotate())
-		.pipe(uglify());
-	
-	// http://stackoverflow.com/questions/26088718/gulp-js-event-stream-merge-order
-	var stream = sq({objectMode: true}, jsStream, tplStream)
-		.pipe(concat('app.min.js'))
-		.pipe(gulp.dest('public/dist'));
-	
-	return stream;
-	
-});
+})
+
+
 
 gulp.task('libs', function() {
 	
 	// if you add more js bower components, add them to this list
 	// and they'll be included in libs.min.js
-	var stream = gulp.src([
+	var libs = [
 		'public/components/jquery/dist/jquery.min.js',
 		'public/components/angular/angular.min.js',
 		'public/components/angular-resource/angular-resource.min.js',
 		'public/components/angular-ui-router/release/angular-ui-router.min.js',
 		'public/components/angular-sanitize/angular-sanitize.min.js',
 		'public/components/lodash/lodash.min.js'
-	])
-		.pipe(concat('libs.min.js'))
-		.pipe(uglify())
-		.pipe(gulp.dest('public/dist'));
+	]
+
+	var stream = gulp.src(libs)                             // get all the lib files
+		.pipe(concat('libs.min.js'))                        // merge them together
+		.pipe(uglify())                                     // minify the js
+		.pipe(gulp.dest('public/dist/'))                    // save it into the dist folder
 	
-	return stream;
+	return stream
 	
-});
+})
 
 
 
-
-
-gulp.task('clean:styles', function(cb){
+gulp.task('reset', function(){
 	
-	del(['public/dist/style.min.css'], cb);
-	
-});
-
-
-
-
-
-gulp.task('clean:scripts', function(cb){
-	
-	del([
-		'public/dist/app.min.js',
-		'public/dist/libs.min.js'
-	], cb);
-	
-});
-
-
-
-gulp.task('reset', function(cb){
 	del([
 		'angular/*/*.js',
 		'server/controllers/*.js',
 		'server/events/*.js',
 		'server/models/*.js',
+		'server/views/*.html',
+		'public/dist/*.css',
+		'public/dist/*.js'
 	], {
 		ignore: [
 			'angular/controllers/mainCtrl.js',
 			'angular/services/$config.js',
-			'angular/services/$socket.js'
+			'angular/services/$socket.js',
+			'angular/views/main.html'
 		]
-	}, cb);
+	})
+	
 })
 
 
 
-
-
-// watch those tasks, and run them once to begin with
-gulp.task('watch', function() {
-
-	gulp.watch(['public/assets/css/*.styl', 'public/assets/css/**/*.styl'], ['styles']);
+gulp.task('watch', ['angular', 'css'], function() {
 	
-	gulp.watch(['angular/**/*.js', 'angular/*.js', 'angular/views/*.html'], ['angular']);
+	gulp.watch(['public/assets/css/*.styl', 'public/assets/css/**/*.styl'], ['css'])
+	gulp.watch(['angular/**/*.js', 'angular/*.js', 'angular/views/*.html'], ['angular'])
+	
+})
 
-});
 
 
-
-
-gulp.task('scripts', ['clean:scripts', 'angular', 'libs'], function(){});
-
-gulp.task('default', ['styles', 'angular', 'libs', 'watch'], function(){
-	console.log('Ready to go!');
-});
-
-gulp.task('clean', ['clean:styles', 'clean:scripts'], function(){});
+gulp.task('default', ['css', 'angular', 'libs'], function(){
+	
+	console.log('Ready to go!')
+	
+})
