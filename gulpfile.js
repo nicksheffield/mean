c8fb8f2b9e2e45deefda420b32fbad47e8eb34b5
Nick Sheffield <numbereft@gmail.com>
2015-10-01 13:28:44 +1300

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,63 @@
 var del           = require('del')                          // delete files
 var gulp          = require('gulp')                         // the main guy
 var jscs          = require('gulp-jscs')                    // javascript code style
+var clone         = require('gulp-clone')                   // used to fork a stream
 var babel         = require('gulp-babel')                   // process es2015 syntax
 var order         = require('gulp-order')                   // reorder files in stream
-var insert        = require('gulp-insert')                  // add text to file
-var rename        = require('gulp-rename')                  // rename file
-var concat        = require('gulp-concat')                  // merge files together
 var uglify        = require('gulp-uglify')                  // minify js
+var rename        = require('gulp-rename')                  // rename file
+var insert        = require('gulp-insert')                  // add text to file
+var concat        = require('gulp-concat')                  // merge files together
 var stylus        = require('gulp-stylus')                  // turn stylus into css
 var addsrc        = require('gulp-add-src')                 // mid-stream gulp.src()
-var plumber       = require('gulp-plumber')                 // handle errors without crashing
-var filelog       = require('gulp-filelog')                 // list all files in the stream
+var notify        = require('gulp-notify')                  // OS-level notifications
 var stylish       = require('gulp-jscs-stylish')            // make jscs output nicer
-var beautify      = require('gulp-beautify')                // make files human readable
+var filelog       = require('gulp-filelog')                 // list all files in the stream
+var plumber       = require('gulp-plumber')                 // handle errors without crashing
 var annotate      = require('gulp-ng-annotate')             // safely minify angular
+var beautify      = require('gulp-cssbeautify')             // make files human readable
 var minifycss     = require('gulp-minify-css')              // minify css code
 var sourcemap     = require('gulp-sourcemaps')              // create sourcemaps
+var combinemq     = require('gulp-combine-media-queries')   // merge all same media queries
 var autoprefix    = require('gulp-autoprefixer')            // prefix any css with low support
 var templateCache = require('gulp-angular-templatecache')   // cache angular template files
 
-var dist = 'public/dist/';
+var paths = {
+	stylus: {
+		files: ['public/assets/css/*.styl'],
+		main: 'public/assets/css/style.styl'
+	},
+	views: ['angular/views/*.html'],
+	angular: ['angular/*.js', 'angular/**/*.js'],
+	output: 'public/dist/'
+}
 
+var plumberOpts = {
+	errorHandler: notify.onError("Error: <%= error.message %>")
+}
 
 gulp.task('css', function() {
 
-	var stream = gulp.src('./public/assets/css/style.styl') // get the source files
-		.pipe(plumber())                                    // stop syntax errors crashing the watch
-		.pipe(sourcemap.init())                             // get ready to write a sourcemap
-		.pipe(stylus())                                     // turn the stylus into css
-		.pipe(sourcemap.write())                            // write the sourcemap
-		.pipe(autoprefix('last 2 versions'))                // autoprefix the css code
-		.pipe(beautify())
-		.pipe(gulp.dest(dist))                              // save it into the dist folder
-		.pipe(minifycss())                                  // minify it (removes the sourcemap)
-		.pipe(sourcemap.write())                            // write the sourcemap
-		.pipe(rename('style.min.css'))                      // add .min to the filename
-		.pipe(gulp.dest(dist))                              // save it into the dist folder
+	// prepare css code
+	var stream = gulp.src(paths.stylus.main)          // grab our stylus file
+		.pipe(plumber(plumberOpts))                   // notify us if any errors appear
+		.pipe(sourcemap.init())                       // get ready to write a sourcemap
+		.pipe(stylus())                               // turn the stylus into css
+		.pipe(combinemq())                            // put all the media queries at the bottom
+		.pipe(sourcemap.write())                      // write the sourcemap
+		.pipe(autoprefix('last 2 versions'))          // autoprefix the css code
+	
+	// make style.css
+	stream.pipe(clone())                              // make a copy of the stream up to autoprefix
+		.pipe(beautify())                             // make css really readable
+		.pipe(gulp.dest(paths.output))                // save it into the dist folder
+	
+	// make style.min.css
+	stream.pipe(clone())                              // make a copy of the stream up to autoprefix
+		.pipe(minifycss())                            // minify it (removes the sourcemap)
+		.pipe(sourcemap.write())                      // write the sourcemap
+		.pipe(rename('style.min.css'))                // add .min to the filename
+		.pipe(gulp.dest(paths.output))                // save it into the dist folder
 	
 	return stream
 
@@ -49,11 +71,11 @@ gulp.task('angular', function() {
 		module: 'app.templates'
 	}
 
-	var stream = gulp.src('angular/views/*.html')           // grab all the html views
+	var stream = gulp.src(paths.views)                      // grab all the html views
 		.pipe(plumber())                                    // stop any errors from breaking a watch
 		.pipe(templateCache('templates.js', tplCacheOpts))  // make a template cache from them
 		.pipe(insert.prepend('//jscs:disable\n'))           // disable jscs for the template cache
-		.pipe(addsrc(['angular/*.js', 'angular/**/*.js']))  // add the rest of the angular app
+		.pipe(addsrc(paths.angular))                        // add the rest of the angular app
 		.pipe(order(['app.js']))                            // make sure app.js is first
 		//.pipe(filelog())                                  // log the files in the stream
 		.pipe(babel())                                      // enable ES2015 support
@@ -63,7 +85,7 @@ gulp.task('angular', function() {
 		.pipe(annotate())                                   // make angular callbacks minifyable
 		.pipe(uglify())                                     // minify the code
 		.pipe(concat('app.min.js'))                         // merge them all into the same file
-		.pipe(gulp.dest(dist))                              // save it into the dist folder
+		.pipe(gulp.dest(paths.output))                      // save it into the dist folder
 		
 	return stream
 	
@@ -87,7 +109,7 @@ gulp.task('libs', function() {
 	var stream = gulp.src(libs)                             // get all the lib files
 		.pipe(concat('libs.min.js'))                        // merge them together
 		.pipe(uglify())                                     // minify the js
-		.pipe(gulp.dest(dist))                              // save it into the dist folder
+		.pipe(gulp.dest(paths.output))                     // save it into the dist folder
 	
 	return stream
 	
@@ -103,8 +125,8 @@ gulp.task('reset', function(){
 		'server/events/*.js',
 		'server/models/*.js',
 		'server/views/*.html',
-		dist + '*.css',
-		dist + '*.js'
+		paths.output + '*.css',
+		paths.output + '*.js'
 	], {
 		ignore: [
 			'angular/controllers/mainCtrl.js',
@@ -120,7 +142,7 @@ gulp.task('reset', function(){
 
 gulp.task('watch', ['angular', 'css'], function() {
 	
-	gulp.watch(['public/assets/css/*.styl', 'public/assets/css/**/*.styl'], ['css'])
+	gulp.watch(paths.stylus.files, ['css'])
 	gulp.watch(['angular/**/*.js', 'angular/*.js', 'angular/views/*.html'], ['angular'])
 	
 })
